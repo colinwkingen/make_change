e94b74824ebeb933b7289c5dcef19d2129c5afab
Colin W Kingen and Sky Rousse <colin@>
2016-06-30 11:29:48 -0700

    update specs to pass

--- spec/make_change_integration_spec.rb
+++ spec/make_change_integration_spec.rb
@@ -22,4 +22,23 @@ describe('the make change path',  {:type => :feature}) do
     There are still 1 quarters in the bank.
     ')
   end
+  it('fills in the input fields and returns the minimum number of coins used and coins still available') do
+    visit('/')
+    fill_in('cents', :with => 267)
+    fill_in('pennies', :with => 2)
+    fill_in('nickles', :with => 1)
+    fill_in('dimes', :with => 1)
+    fill_in('quarters', :with => 10)
+    click_button('Submit')
+    expect(page).to have_content('You got 2 pennies back
+    You got 1 nickles back
+    You got 1 dimes back
+    You got 10 quarters back
+    There are still 0 pennies in the bank.
+    There are still 0 nickles in the bank.
+    There are still 0 dimes in the bank.
+    There are still 0 quarters in the bank.
+    ')
+#save_and_open_page
+  end
 end
